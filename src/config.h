99cb651634f10004093b576a6a1ccc9cdb89bfa6
Eigil Skjæveland <eigil@skjvlnd.com>
2015-07-20 15:23:03 +0200

    Separated config header

---
+++ src/config.h
@@ -0,0 +1,8 @@
+#define STAT "/proc/stat"
+#define LOAD "/proc/loadavg"
+#define MEM  "/proc/meminfo"
+#define TEMP "/sys/class/hwmon/"
+#define BATT "/sys/class/power_supply/BAT0/"
+#define AC   "/sys/class/power_supply/AC/"
+
+#define WIFACE "wlan0"
